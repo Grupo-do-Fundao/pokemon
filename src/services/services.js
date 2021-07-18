8c9ada03a477355f7356982984da727472e060be
lucasnahas <lucasahas@gmail.com>
2021-07-17 23:45:56 -0300

    Melhora a logica de carregamento

--- src/services/services.js
+++ src/services/services.js
@@ -5,20 +5,20 @@ async function fetchApi(id) {
   return response;
 }
 
-export default async function loadPokemons() {
-
+function loadLocalStorage() {
   const storageContent = localStorage.getItem("pokemonApp");  
-  if (storageContent){
+  if (storageContent !== null) {
+    console.log('LOCAL STORAGE');
     const pokemons = JSON.parse(storageContent);
-    console.log('Local Storage');
     return pokemons;
   }
-  
-  let pokemons = [];
-  for (let i = 1; i <= 20; i++){
-    const rawData = await fetchApi(i)
-    const { id, name, moves, types, stats } = rawData;
-    const pokemon = {
+  return false;
+}
+
+function createPokemonObj(rawData) {
+  const { id, name, moves, types, stats } = rawData;
+  return (
+    {
       id,
       name,
       moves,
@@ -30,13 +30,37 @@ export default async function loadPokemons() {
         [stats[3]['stat']['name']]: stats[3]['base_stat'],
         [stats[4]['stat']['name']]: stats[4]['base_stat'],
         [stats[5]['stat']['name']]: stats[5]['base_stat'],
-      }
+      },
+      party: false,
+      favorite: false,
     }
+  );
+}
+
+async function fetchAllPokemons(num) {
+  let pokemons = [];
+  for (let i = 1; i <= num; i++){
+    const rawData = await fetchApi(i);
+    const pokemon = createPokemonObj(rawData);    
     pokemons = [...pokemons, pokemon];
   }
-  const save = JSON.stringify(pokemons)
-  localStorage.setItem("pokemonApp", save);
-  
+  return pokemons;
+}
+
+function saveInLocalStorage(pokemons) {
+  if (typeof pokemons === 'object' && pokemons.length > 0 && typeof pokemons[0] === 'object' && pokemons[0].party === false) {
+    const save = JSON.stringify(pokemons)
+    localStorage.setItem("pokemonApp", save);
+  } else {
+    throw new Error('POKEMON APP: ERRO AO SALVAR NO LOCAL STORAGE');
+  }
+}
+
+export default async function loadPokemons() {
+  let pokemons = loadLocalStorage();
+  if (pokemons) return pokemons;
+  pokemons = await fetchAllPokemons(4);
+  saveInLocalStorage(pokemons);
   console.log('API');
   return pokemons;
 }
